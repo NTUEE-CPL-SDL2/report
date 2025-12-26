2cbcd38ff822a0d31646d7fd323d2ac09322b79a
Willie169 <cg10250207@gmail.com>
2025-12-26 16:44:25 +0800

    x

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
-java -jar "$HOME/plantuml.jar" -tsvg diagrams/example_class_diagram.puml
+java -jar "$HOME/plantuml.jar" -tsvg diagrams/full.puml
+java -jar "$HOME/plantuml.jar" -tsvg diagrams/exclude.puml
+java -jar "$HOME/plantuml.jar" -tsvg diagrams/mystd.puml
+java -jar "$HOME/plantuml.jar" -tsvg diagrams/array.puml
 
-SVG="diagrams/example_class_diagram.svg"
+DPI=300
+
+SVG="diagrams/full.svg"
 
 F_WIDTH=$(inkscape "$SVG" --query-width | sed 's/px//')
 F_HEIGHT=$(inkscape "$SVG" --query-height | sed 's/px//')
@@ -16,7 +21,6 @@ inkscape "$SVG" \
   --export-type=png \
   --export-filename="diagrams/example_class_diagram.png"
 
-DPI=300
 P_WIDTH=$(echo "210 * $DPI / 25.4" | bc -l)
 P_HEIGHT=$(echo "297 * $DPI / 25.4" | bc -l)
 W_PAGES=$(echo "($F_WIDTH + $P_WIDTH - 1) / $P_WIDTH" | bc)
@@ -33,6 +37,70 @@ for ((i=0; i<W_PAGES; i++)); do
       --export-dpi=$DPI \
       --export-type=png \
       --export-filename="diagrams/page_w${i}_h${j}.png"
+  done
+done
+
+SVG="diagrams/exclude.svg"
+
+F_WIDTH=$(inkscape "$SVG" --query-width | sed 's/px//')
+F_HEIGHT=$(inkscape "$SVG" --query-height | sed 's/px//')
+C_WIDTH=$(awk -v n="$F_WIDTH" 'BEGIN {print (n==int(n)?n:int(n)+1)}')
+C_HEIGHT=$(awk -v n="$F_HEIGHT" 'BEGIN {print (n==int(n)?n:int(n)+1)}')
+
+inkscape "$SVG" \
+  --export-area=0:0:$C_WIDTH:$C_HEIGHT \
+  --export-dpi=96 \
+  --export-type=png \
+  --export-filename="diagrams/example_class_diagram.png"
+
+P_WIDTH=$(echo "210 * $DPI / 25.4" | bc -l)
+P_HEIGHT=$(echo "297 * $DPI / 25.4" | bc -l)
+W_PAGES=$(echo "($F_WIDTH + $P_WIDTH - 1) / $P_WIDTH" | bc)
+H_PAGES=$(echo "($F_HEIGHT + $P_HEIGHT - 1) / $P_HEIGHT" | bc)
+
+for ((i=0; i<W_PAGES; i++)); do
+  x0=$(echo "$i * $P_WIDTH" | bc -l)
+  x1=$(echo "($i + 1) * $P_WIDTH" | bc -l)
+  for ((j=0; j<H_PAGES; j++)); do
+    y0=$(echo "$j * $P_HEIGHT" | bc -l)
+    y1=$(echo "($j + 1) * $P_HEIGHT" | bc -l)
+    inkscape "$SVG" \
+      --export-area="$x0:$y0:$x1:$y1" \
+      --export-dpi=$DPI \
+      --export-type=png \
+      --export-filename="diagrams/ex_page_w${i}_h${j}.png"
+  done
+done
+
+SVG="diagrams/mystd.svg"
+
+F_WIDTH=$(inkscape "$SVG" --query-width | sed 's/px//')
+F_HEIGHT=$(inkscape "$SVG" --query-height | sed 's/px//')
+C_WIDTH=$(awk -v n="$F_WIDTH" 'BEGIN {print (n==int(n)?n:int(n)+1)}')
+C_HEIGHT=$(awk -v n="$F_HEIGHT" 'BEGIN {print (n==int(n)?n:int(n)+1)}')
+
+inkscape "$SVG" \
+  --export-area=0:0:$C_WIDTH:$C_HEIGHT \
+  --export-dpi=96 \
+  --export-type=png \
+  --export-filename="diagrams/example_class_diagram.png"
+
+P_WIDTH=$(echo "210 * $DPI / 25.4" | bc -l)
+P_HEIGHT=$(echo "297 * $DPI / 25.4" | bc -l)
+W_PAGES=$(echo "($F_WIDTH + $P_WIDTH - 1) / $P_WIDTH" | bc)
+H_PAGES=$(echo "($F_HEIGHT + $P_HEIGHT - 1) / $P_HEIGHT" | bc)
+
+for ((i=0; i<W_PAGES; i++)); do
+  x0=$(echo "$i * $P_WIDTH" | bc -l)
+  x1=$(echo "($i + 1) * $P_WIDTH" | bc -l)
+  for ((j=0; j<H_PAGES; j++)); do
+    y0=$(echo "$j * $P_HEIGHT" | bc -l)
+    y1=$(echo "($j + 1) * $P_HEIGHT" | bc -l)
+    inkscape "$SVG" \
+      --export-area="$x0:$y0:$x1:$y1" \
+      --export-dpi=$DPI \
+      --export-type=png \
+      --export-filename="diagrams/my_page_w${i}_h${j}.png"
   done
 done
 
